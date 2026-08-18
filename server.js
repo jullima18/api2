d46bbaffdf7045572ce7aa6e5adf1b48aeea7acc
jullima18 <julia.vilanova@aluno.senai.br>
2026-08-18 12:26:15 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express');
 
+const cors = require('cors')
+
 const app = express();
+
+
+const corsOptions = {
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
+}
+
+app.use(cors(corsOptions))
+
 app.use(express.json());
 
-app.get('/alunos',(reg,res)=>{
+const alunos = [
+    { "nome": "julia", "idade": 18 },
+    { "nome": "emily", "idade": 20 },
+    { "nome": "maisa", "idade": 25 }
+
+]
+
+app.get('/alunos', (reg, res) => {
     res.send("lendo dados do banco")
 })
+
+
+
+app.post("/alunos", (req, res) => {
+
+    const { nome, url_imagem } = req.body
+
+    console.log(nome+" "+url_imagem)
+
+
+
+    //res.send(`nome ${req.body.nome} Idade:${req.body.url_imagem}`)
+})
+
+
+
 
 app.listen(3000, () => {
     console.log('servidor rodando na porta 3000');
 });
-
-app.post("/alunos",(req, res)=>{
-  res.send(`nome ${req.body.nome} idade:${req.body.idade}`)
-})
-
-const alunos =[
-{"nome":"julia","idade":18}
-{"nome":"emily","idade":20}
-{"nome":,"maisa","idade":25}
-
-]
